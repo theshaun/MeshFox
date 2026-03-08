27d3a29896178e816b5ce18fe7eb1076ec632eeb
theshaun <shaun@theshaun.com>
2026-03-08 14:27:43 +1000

    rework usage of luckfox-pico using new repos

--- environment-setup/meshfox-builder-pico.sh
+++ environment-setup/meshfox-builder-pico.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# MeshFox builder script
+# MeshFox builder script for Luckfox Pico
 # Run this to set up your build environment and build the image. 
-# You can also run this script with different arguments to just build certain steps, for example `./meshfox-builder.sh build_rootfs` to just build the rootfs. 
-# Run `./meshfox-builder.sh help` for more information. 
+# You can also run this script with different arguments to just build certain steps, for example `./meshfox-builder-pico.sh build_rootfs` to just build the rootfs. 
+# Run `./meshfox-builder-pico.sh help` for more information. 
 #
 # Based on Foxbuntu by Femtofox (https://github.com/femtofox/femtofox)
 
@@ -64,12 +64,7 @@ clone_repos() {
     fi
   }
 
-  # todo: determine switch to upstream luckfox-pico, also when other luckfox boards are brought in, support for those
-  # The luckfox lyra seems to be just on an SDK download https://drive.google.com/drive/folders/1l2ixhfw53J3eZunyvHMnw2DYViH9d8Cx?usp=drive_link
-  # We should re-host this on github if possible
-
-  #clone_with_retries "https://github.com/LuckfoxTECH/luckfox-pico.git" || return 1
-  clone_with_retries "https://github.com/Ruledo/luckfox-pico.git" || return 1
+  clone_with_retries "https://github.com/theshaun/luckfox-pico.git" || return 1
   clone_with_retries "https://github.com/theshaun/MeshFox.git" || return 1
 
   return 0
@@ -77,7 +72,7 @@ clone_repos() {
 
 build_env() {
   echo "Setting up SDK env..."
-  echo "When the menu appears to choose your board choose Luckfox Pico Mini A (1), SDCard (0) and Ubuntu (1)."
+  echo "When the menu appears to choose your board choose Luckfox Pico Mini (1), SDCard (0) and Ubuntu (1)."
   echo "Press any key to continue building the environment..."
   read -n 1 -s -r
   cd /home/${sudoer}/luckfox-pico
@@ -175,26 +170,8 @@ EOF
   create_image
 }
 
-modify_chroot() {
-  echo "Entering chroot... make your changes and then type exit when you are done and it will build the image with your changes."
-  echo "Press any key to continue entering chroot..."
-  read -n 1 -s -r
-  mount --bind /proc /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/proc
-  mount --bind /sys /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/sys
-  mount --bind /dev /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/dev
-  mount --bind /dev/pts /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/dev/pts
-  chroot /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106 /bin/bash
-  umount /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/dev/pts
-  umount /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/proc
-  umount /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/sys
-  umount /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/dev
-  build_rootfs
-  build_firmware
-  create_image
-}
-
 rebuild_chroot() {
-  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox.chroot}
+  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox-core.chroot}
   if [[ ! -f $chroot_script ]]; then
     echo "Error: Chroot script $chroot_script not found."
     exit 1
@@ -208,7 +185,7 @@ rebuild_chroot() {
   rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/ /home/${sudoer}/luckfox-pico/sysdrv/
   rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/project/ /home/${sudoer}/luckfox-pico/project/
   build_rootfs
-  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs_uclibc_rv1106/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
+  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
   build_firmware
   install_rootfs
   build_rootfs
@@ -217,7 +194,7 @@ rebuild_chroot() {
 }
 
 inject_chroot() {
-  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox.chroot}
+  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox-core.chroot}
   if [[ ! -f $chroot_script ]]; then
     echo "Error: Chroot script $chroot_script not found."
     exit 1
@@ -266,7 +243,7 @@ full_rebuild() {
   sync_meshfox_changes
   build_kernelconfig
   build_rootfs
-  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs_uclibc_rv1106/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
+  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
   build_firmware
   install_rootfs
   build_rootfs
@@ -282,7 +259,7 @@ install_rootfs() {
   cp /home/${sudoer}/luckfox-pico/sysdrv/out/kernel_drv_ko/* /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/lib/modules/5.10.160/
   which qemu-arm-static
 
-  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox.chroot}
+  chroot_script=${CHROOT_SCRIPT:-/home/${sudoer}/meshfox/environment-setup/meshfox-core.chroot}
   if [[ ! -f $chroot_script ]]; then
     echo "Error: Chroot script $chroot_script not found."
     exit 1
@@ -362,7 +339,7 @@ sdk_install() {
   sync_meshfox_changes
   build_kernelconfig
   build_rootfs
-  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs_uclibc_rv1106/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
+  rsync -aHAXv --progress --keep-dirlinks --itemize-changes /home/${sudoer}/meshfox/meshfox/sysdrv/out/rootfs/ /home/${sudoer}/luckfox-pico/sysdrv/out/rootfs_uclibc_rv1106/
   build_firmware
   install_rootfs
   build_rootfs
@@ -379,12 +356,10 @@ sdk_install() {
 usage() {
   echo "The following functions are available in this script:"
   echo "To install the development environment use the arg 'sdk_install' and is intended to be run ONCE only."
-  echo "To modify the chroot and build an updated image use the arg 'modify_chroot'."
   echo "To modify the kernel and build an updated image use the arg 'modify_kernel'."
   echo "To specify a custom chroot script use the arg '--chroot-script /full/path/to/custom.chroot'"
   echo "other args: full_rebuild rebuild_chroot inject_chroot build_env sync_meshfox_changes build_kernelconfig install_rootfs build_rootfs build_uboot build_firmware create_image"
   echo "Example:  sudo ~/foxbunto_env_setup.sh sdk_install"
-  echo "Example:  sudo ~/foxbunto_env_setup.sh modify_chroot"
   echo "Example:  sudo ~/foxbunto_env_setup.sh --chroot-script /home/user/custom.chroot"
   exit 0
 }
@@ -414,16 +389,15 @@ elif [[ -z ${1} ]]; then
       1 "Full Image Rebuild" \
       2 "Get Image Updates" \
       3 "Modify Kernel Menu" \
-      4 "Enter and Modify Chroot" \
-      5 "Rebuild Chroot" \
-      6 "Inject Chroot Script (CAUTION)" \
-      7 "Manual Build Environment" \
-      8 "Manual Build U-Boot" \
-      9 "Manual Build RootFS" \
-      10 "Manual Build Firmware" \
-      11 "Manual Create Final Image" \
-      12 "SDK Install (Run this first.)" \
-      13 "Exit" \
+      4 "Rebuild Chroot" \
+      5 "Inject Chroot Script (CAUTION)" \
+      6 "Manual Build Environment" \
+      7 "Manual Build U-Boot" \
+      8 "Manual Build RootFS" \
+      9 "Manual Build Firmware" \
+      10 "Manual Create Final Image" \
+      11 "SDK Install (Run this first.)" \
+      12 "Exit" \
       2>&1 >/dev/tty)
 
     clear
@@ -432,16 +406,15 @@ elif [[ -z ${1} ]]; then
       1) full_rebuild ;;
       2) update_image ;;
       3) modify_kernel ;;
-      4) modify_chroot ;;
-      5) rebuild_chroot ;;
-      6) inject_chroot ;;
-      7) build_env ;;
-      8) build_uboot ;;
-      9) build_rootfs ;;
-      10) build_firmware ;;
-      11) create_image ;;
-      12) sdk_install ;;
-      13) echo "Exiting..."; break ;;
+      4) rebuild_chroot ;;
+      5) inject_chroot ;;
+      6) build_env ;;
+      7) build_uboot ;;
+      8) build_rootfs ;;
+      9) build_firmware ;;
+      10) create_image ;;
+      11) sdk_install ;;
+      12) echo "Exiting..."; break ;;
       *) echo "Invalid option, please try again." ;;
     esac
 
